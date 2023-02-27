f9ba0c222ee80c72c3d430b6d3f612c69e99c490
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-27 23:09:29 +0530

    commit

--- src/main/java/assignment/bank_new/BankAccount.java
+++ src/main/java/assignment/bank_new/BankAccount.java
@@ -46,36 +46,4 @@ public class BankAccount{
     }
 }
 
-    
-class BankOperation{
-	String holdername;
-    long accno;
-    double balance = 0;
-    Scanner inputobj = new Scanner(System.in);
-    Logger log=Logger.getLogger("hi");
-	BankOperation(String holdername,long accno){
-		this.holdername =holdername;
-		this.accno = accno;
-		balance = 0;
-	}
-	void deposit() {      
-        log.info("Enter deposit Amount");
-        double amount = inputobj.nextDouble();
-        balance = balance + amount;
-    }
-
-    void withDraw() {
-        log.info("Enter Withdraw Amount");
-        double withdrawamt = inputobj.nextDouble();
-        if (withdrawamt > balance) {
-            log.info("Thers is no required amount");
-        } else {
-            balance = balance - withdrawamt;
-        }
-    }
-
-    void balanceAmt() {
-        log.log(Level.INFO,()->"Balance is" + balance);
-    }
-}
 	
